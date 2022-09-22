16cbc2e74a0b479adf6f236f4c1d3789bb899a32
Dhruvika Sharma <dhruvikasharma20@gmail.com>
2022-09-22 17:14:55 +0530

    Update train.py

--- train.py
+++ train.py
@@ -34,12 +34,12 @@ train_datagen = ImageDataGenerator(
         horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(
-        'dataset_path',
+        dataset_path,
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 validation_generator = test_datagen.flow_from_directory(
-        'dataset_path',
+        dataset_path,
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
